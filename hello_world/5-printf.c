a611e10c79b81ce2f6a8c5521fa0e400edc0376f
Luismi <l.gallogomez@gmail.com>
2023-02-23 12:29:31 -0800

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
+/**
+*main - prints "with proper grammar, but the outcome is a piece of art,"
+*Return: 0
+*/
+
 #include <stdio.h>
 
-int main () {
-	printf("Mocos");
-	return 0;
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return 0;
 }
