87386082e477f843cb62837649561978071f79b9
Luismi <l.gallogomez@gmail.com>
2023-02-25 09:49:39 -0800

    adding \n), n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 1)
 {
-printf("%d is positive");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero");
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative");
+printf("%d is negative\n", n);
 }
     
 
