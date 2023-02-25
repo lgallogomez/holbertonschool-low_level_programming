64e88451431ce70270cddb563087c4e0e406fe98
Luismi <l.gallogomez@gmail.com>
2023-02-25 15:12:43 -0800

    quitting spaces

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+
 
 /**
  * * main - for a random number
  *  * return: 0
  */
 
-/* betty style doc for function main goes there */
 int main(void)
+
 {
 int n;
 
