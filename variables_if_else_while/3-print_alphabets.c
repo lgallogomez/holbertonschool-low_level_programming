7ae6f11a393caab2aa93e96bc2db5f4e493474bc
Luismi <l.gallogomez@gmail.com>
2023-02-26 14:13:15 -0800

    3th

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-/*
- ** print lower and upper case alphabet with putchar
- * *Return 0
+/**
+* print lower and upper case alphabet with putchar
+* *Return 0
 */
 
 int main(void)
 {
 char ch = 'a';
-while(ch <= 'z') {
+while(ch <= 'z')
+{
 putchar(ch);
 ch++;
 }
 {
 char chupp = 'A';
-while(chupp <= 'Z');
+while(chupp <= 'Z')
 putchar(chupp);
 chupp++;
 }
