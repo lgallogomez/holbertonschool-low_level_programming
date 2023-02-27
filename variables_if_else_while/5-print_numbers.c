e45bed4b55911bebd4c616d4acbde5a27b7f0c66
Luismi <l.gallogomez@gmail.com>
2023-02-26 16:30:15 -0800

    5th

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 /**
- * main - Prints digits
- * Return: 0
- */
+* main - Prints digits
+* Return: 0
+*/
 #include <stdio.h>
 int main(void)
 {
-  unsigned short d;
-  d = 0;
-  while (d < 10)
-    {
-      printf("%hu", d);
-      d++;
-    }
-  putchar('\n');
-  return (0);
+unsigned short d;
+d = 0;
+while (d < 10)
+{
+printf("%hu", d);
+d++;
+}
+putchar('\n');
+return (0);
 }
