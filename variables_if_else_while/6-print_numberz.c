f541565762d000cc6c303f288e050ef9274a77b8
Luismi <l.gallogomez@gmail.com>
2023-02-26 16:33:45 -0800

    all

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 /**
- * main - Prints numbers
- * Return: 0
- */
+* main - Prints numbers
+* Return: 0
+*/
 #include <stdio.h>
 int main(void)
 {
-  int c;
-  c = '0';
-  while (c <= '9')
-    {
-      putchar(c);
-      c++;
-    }
-  putchar('\n');
-  return (0);
+int c;
+c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
