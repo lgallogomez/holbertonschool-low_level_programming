bb651f2a654b0e438b541b996810f5f6a343c7f4
Luismi <l.gallogomez@gmail.com>
2023-03-01 16:39:35 -0800

    4th

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 /**
 *_isalpha - checks if input is alphabet
 *@c: the input to check
-*return: 1 if is a letter, 0 if not.
+*Return: 1 if is a letter, 0 if not.
 */
 #include "main.h"
 
-int _isalpha (int c)
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
