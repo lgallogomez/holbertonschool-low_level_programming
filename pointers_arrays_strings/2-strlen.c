58baabcefebd58a7e0398dfb703b4d31268edabe
Luismi <l.gallogomez@gmail.com>
2023-03-02 12:55:59 -0800

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -10,12 +10,11 @@
 
 int _strlen(char *s)
 {
-    int length = 0;
+int length = 0;
 
-    while (*s != '\0') {
-        length++;
-        s++;
-    }
-
-    return length;
+while (*s != '\0') {
+length++;
+s++;
+}
+return length;
 }
