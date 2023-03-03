85b4c2408f2b2224ac7abc34b4b8b5a4d3c75aba
Luismi <l.gallogomez@gmail.com>
2023-03-03 13:25:58 -0800

    5th

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,6 +4,8 @@
  *
  * Return: 1 if positive, -1 if negative, 0 if zero
  */
+
+#include "main.h"
 int print_sign(int n)
 {
 	if (n > 0)
@@ -11,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -22,4 +24,3 @@ int print_sign(int n)
 		return (-1);
 	}
 }
-
