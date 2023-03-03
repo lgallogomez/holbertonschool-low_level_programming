7f86f76db8548af29bb9bce1fdeac8a39ac9810a
Luismi <l.gallogomez@gmail.com>
2023-03-03 13:48:20 -0800

    6th

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - returns absolute value
+ * @n:int argument to evaluate
+ * Return: Absolute value of n
+ */
+
+#include "main.h"
+int _abs(int n)
+{
+	if (n > 0)
+	return (n);
+else
+	return (n * -1);
+}
