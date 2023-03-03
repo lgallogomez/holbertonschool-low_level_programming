7e8081308336de74f1738363949ea0cbea7496c2
Luismi <l.gallogomez@gmail.com>
2023-03-03 13:13:30 -0800

    5th

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/**
+ * print_sign - prints whether a number is positive, negative or zero
+ * @n: the number to check
+ *
+ * Return: 1 if positive, -1 if negative, 0 if zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
+}
+
