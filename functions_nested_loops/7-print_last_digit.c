aaee9015449a13760bd2dc892d1e97911d7f5ae8
Luismi <l.gallogomez@gmail.com>
2023-03-03 14:16:36 -0800

    7th

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,8 @@
 /**
  *print_last_digit - does it by using the remainder when div by 10
  *@n : argument to print the last digit
- *return : value of last digit
+ *Return : value of last digit
  */
-
 #include "main.h"
 #include <stdlib.h>
 int print_last_digit(int n)
