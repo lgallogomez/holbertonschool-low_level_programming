93ebddfaba6c1bff4df8b59b9527640c523ee7c6
Luismi <l.gallogomez@gmail.com>
2023-03-03 14:17:50 -0800

    7th

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,10 +5,11 @@
  */
 #include "main.h"
 #include <stdlib.h>
+
 int print_last_digit(int n)
 {
-	char t;
-	t = ((abs(n % 10)) + '0');
-	_putchar(t);
-	return (abs(n % 10));
+char t;
+t = ((abs(n % 10)) + '0');
+_putchar(t);
+return (abs(n % 10));
 }
