610c9b69a03be19963230ddb1f709e92182d3bd9
Luismi <l.gallogomez@gmail.com>
2023-03-02 16:11:54 -0800

    3th

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 /*
- *_puts - prints a string char by char
- *@str: the pointer that receives the value
- */
+*_puts - prints a string char by char
+*@str: the pointer that receives the value
+*/
 
 #include "main.h"
 
@@ -9,7 +9,7 @@ void _puts(char *str)
 {
 	int i = 0;
 	while (str[i] != '\0')
-	{
+{
 	_putchar(str[i]);
 	i++;
 }
