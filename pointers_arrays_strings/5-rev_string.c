54fee89f086be836e5409ec9afa76f86b1ff6ba9
Luismi <l.gallogomez@gmail.com>
2023-03-03 06:49:06 -0800

    5th

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ * rev_string - print reverse characters.
+ * @s: validate the character
+ * Return: Always 0.
+ */
+
+#include "main.h"
+void rev_string(char *s)
+{
+	int i = 0, a = 0, Aux;
+
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > a)
+	{
+		Aux = s[i];
+		s[i--] = s[a];
+		s[a++] = Aux;
+	}
+}
