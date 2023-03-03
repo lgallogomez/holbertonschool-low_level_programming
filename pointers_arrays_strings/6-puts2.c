bbf99124cdb4843d9e89ca202b0d36477d6ae216
Luismi <l.gallogomez@gmail.com>
2023-03-03 07:10:21 -0800

    6th

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+/**
+*puts2 - print every other char of a str 1st line and followed by new line
+*@*str
+*/
+
+#include "main.h"
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		putchar(str[i];
+		i += 2;
+	}
+
+	putchar('\n');
+}
