008ec1b29e254f6590f78abbe6a71e78585bb94a
Luismi <l.gallogomez@gmail.com>
2023-03-06 14:07:36 -0800

    10th

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ *add - add two numbers
+ *@a : first value to add
+ *@b : 2nd value to add
+ *Return: the sum of those
+/*
+
+#include "main.h"
+#include <stdio.h>
+
+int add(int a, int b)
+{
+return (a + b);
+}
