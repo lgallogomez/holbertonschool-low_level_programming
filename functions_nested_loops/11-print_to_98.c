9b3cef3103194c401508e236c4729943f99adb85
Luismi <l.gallogomez@gmail.com>
2023-03-06 14:19:35 -0800

    11th

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+/**
+ *print_to_98 - takes input from user and prints natural # until 98
+ *@n : number where print begins
+ *Return: 0
+ */
+
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+    int i;
+    if (n > 98) {
+        for (i = n; i >= 98; i--) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (i = n; i <= 98; i++) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
+}
