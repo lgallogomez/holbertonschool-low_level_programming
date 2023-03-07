7608490f8ba626892ddcc088bfd499b20f39a7b1
Luismi <l.gallogomez@gmail.com>
2023-03-07 15:30:16 -0800

    6th

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 
 void more_numbers(void)
 {
-    int i, j;
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 0; i <= 14; i++)
-	{
-            _putchar(i < 10 ? i + '0' : '1');
-            if (i >= 10) _putchar(i % 10 + '0');
-        }
-        _putchar('\n');
-    }
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+}
+_putchar(i < 10 ? i + '0' : '1');
+if (i >= 10) _putchar(i % 10 + '0');
+}
+_putchar('\n');
+}
 }
