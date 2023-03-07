cecfaaf1de9419630947d9fb022d55103be8c8f1
Luismi <l.gallogomez@gmail.com>
2023-03-07 15:24:23 -0800

    6th

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,12 @@
 
 void more_numbers(void) {
     int i, j;
-    for (j = 0; j < 10; j++) { // loop 10 times to print each number 10 times
-        for (i = 0; i <= 14; i++) { // loop through the numbers 0 to 14
-            putchar(i + '0'); // print the current number as a character
-        }
-        putchar('\n'); // move to a new line after printing each number 10 times
+    for (j = 0; j < 10; j++)
+    {
+	    for (i = 0; i <= 14; i++)
+	    {
+            putchar(i + '0');
+	    }
+        putchar('\n');
     }
 }
