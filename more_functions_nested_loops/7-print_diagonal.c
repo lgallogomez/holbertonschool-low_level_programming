aa72bf9fbf98d9ea56a42aff2e842b6d0abd8543
Luismi <l.gallogomez@gmail.com>
2023-03-08 17:37:49 -0800

    8th

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,21 +8,21 @@
 #include "main.h"
 void print_diagonal(int n)
 {
-    int i, j;
+int i, j;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < i; j++)
-        {
-            _putchar(' ');
-        }
-        _putchar('\\');
-        _putchar('\n');
-    }
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
 }
