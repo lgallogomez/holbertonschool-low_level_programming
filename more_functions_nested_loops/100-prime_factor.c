487d0938c2c7f855a8cf0ad67907f38ed5927271
Luismi <l.gallogomez@gmail.com>
2023-03-10 13:23:47 -0800

    12th

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+/**
+ * main - find & prints the largest prime factor of a #
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main() {
+    long long n = 612852475143;
+    int i;
+
+    while (n % 2 == 0) {
+        n /= 2;
+    }
+
+    for (i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            n /= i;
+        }
+    }
+
+    if (n > 2) {
+        printf("%lld\n", n);
+    }
+
+    return 0;
+}
