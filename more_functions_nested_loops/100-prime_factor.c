31ec0bbc3c5849d12df865c9d72310b370cb5fba
Luismi <l.gallogomez@gmail.com>
2023-03-10 13:26:46 -0800

    12th

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -5,21 +5,26 @@
 
 #include <stdio.h>
 
-int main() {
+int main(void)
+{
     long n = 612852475143;
     int i;
 
-    while (n % 2 == 0) {
+    while (n % 2 == 0)
+    {
         n /= 2;
     }
 
-    for (i = 3; i * i <= n; i += 2) {
-        while (n % i == 0) {
+    for (i = 3; i * i <= n; i += 2)
+    {
+        while (n % i == 0)
+	{
             n /= i;
         }
     }
 
-    if (n > 2) {
+    if (n > 2)
+    {
         printf("%ld\n", n);
     }
 
