0312d68d473325458d66a0f5518719dcc9a3d041
Luismi <l.gallogomez@gmail.com>
2023-03-10 13:28:11 -0800

    12th

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -7,26 +7,26 @@
 
 int main(void)
 {
-    long n = 612852475143;
-    int i;
+long n = 612852475143;
+int i;
 
-    while (n % 2 == 0)
-    {
-        n /= 2;
-    }
+while (n % 2 == 0)
+{
+n /= 2;
+}
 
-    for (i = 3; i * i <= n; i += 2)
-    {
-        while (n % i == 0)
-	{
-            n /= i;
-        }
-    }
+for (i = 3; i * i <= n; i += 2)
+{
+while (n % i == 0)
+{
+n /= i;
+}
+}
 
-    if (n > 2)
-    {
-        printf("%ld\n", n);
-    }
+if (n > 2)
+{
+printf("%ld\n", n);
+}
 
-    return 0;
+return 0;
 }
