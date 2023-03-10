a396c9afa1a7deadc643640f4a3afccfefad7279
Luismi <l.gallogomez@gmail.com>
2023-03-10 12:11:55 -0800

    9th

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,19 @@
 void print_square(int size)
 {
 	int i, j;
+
+
+	if(size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
+		{
 			_putchar('#');
-		_putchar('\n');
+		}
 	}
-	if(size <= 0)
-		    {
-			    _putchar('\n');
-		    }
-	_putchar('\n');
+		_putchar('\n');
 }
