1ac1e30ac97b304b5e61edbfe546cddca4c4fb53
lgallogomez <l.gallogomez@gmail.com>
2023-06-23 12:27:17 -0700

    1- prints last digit of a random number and a text- changed if conditional from n to last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,12 +11,13 @@ int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n , last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n",n,last_digit);
 	}
 	else if (last_digit == 0)
 	{
