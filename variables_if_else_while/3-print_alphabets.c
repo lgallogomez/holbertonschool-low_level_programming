12104d2cd860d7e62020b81042d8a04fef8d522d
lgallogomez <l.gallogomez@gmail.com>
2023-06-23 14:40:31 -0700

    3- prints alphabet in lower, then in uppercase followed by a newline using putchar c function and while loop

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+/**
+ *main - prints alph in lower and then in upper
+ *Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char a = 97;
+	char b = 65;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b < 91)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+}
