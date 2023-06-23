ab6975a3cbcaba81df3c0a55aa3ffde0cbcab77f
lgallogomez <l.gallogomez@gmail.com>
2023-06-23 09:03:17 -0700

    0- c program takes random number and prints if is +, 0 or -

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  *main - assign a random number and prints if +, 0 or -
@@ -8,15 +9,15 @@
  */
 int main(void)
 {
-int n:
+  int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
  if (n > 0)
-   printf("%s is positive\n", n);
+   printf("%d is positive\n", n);
  else if (n = 0)
-   printf("%s is zero\n", n);
+   printf("%d is zero\n", n);
  else
-   printf("%s is negative\n", n);
+   printf("%d is negative\n", n);
 return (0);
 }
