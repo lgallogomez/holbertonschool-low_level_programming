d5a7b7311dd057672b581b752208b01c03fb5b41
lgallogomez <l.gallogomez@gmail.com>
2023-06-27 15:31:50 -0700

    function prints checks if char is lowercase based on ascii code

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/**
+ *_islower - checks if char is lowercase
+ *Return: 1 if char is lowercase, else 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
