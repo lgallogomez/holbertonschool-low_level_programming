9393fa825cbadf72669d8dc463f0001098dc443e
lgallogomez <l.gallogomez@gmail.com>
2023-06-27 13:59:26 -0700

    _putchar to every char until lcompleting _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
 /**
- * main - prints putchar
- *Return: 0
+ * main - prints every char until _putchar
+ * _putchar - prints a char
+ * Return: 0
  */
 
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 int main(void)
 {
-	_putchar("_putchar", '\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
