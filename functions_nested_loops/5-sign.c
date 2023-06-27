e260e25c5e10fab701ec6a8beee9bf472ef24a47
lgallogomez <l.gallogomez@gmail.com>
2023-06-27 16:48:58 -0700

    quit ascii codes and trying with literal numbers in each case

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@
 
 int print_sign(int n)
 {
-	if (n >= 49 && n <= 57)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 48)
+	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar(0);
 		return (0);
 	}
 	else
