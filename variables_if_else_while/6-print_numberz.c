de3f34ddc67c66e1a6d1cf145806e960ec2547b6
lgallogomez <l.gallogomez@gmail.com>
2023-06-26 18:23:50 -0700

    6 - prints sigle digit nums with outchar and ascii

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ *main - prints all single digit # from 0.
+ *Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+        int single_dig_nums = 48;
+
+        while (single_dig_nums <= 57)
+        {
+                putchar(single_dig_nums);
+                single_dig_nums++;
+        }
+        putchar ('\n');
+        return (0);
+}
