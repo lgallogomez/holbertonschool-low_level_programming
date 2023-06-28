f72533d415e3159a0a267527803207f78668e224
lgallogomez <l.gallogomez@gmail.com>
2023-06-27 17:23:37 -0700

    functions created to compute absolute value of integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+ *_abs - computes absolutes value of integer
+ *@: number to compute absolut value
+ *Return: absolute number
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
