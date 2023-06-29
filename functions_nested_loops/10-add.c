2f93ee982a437c1f6f6bcaf89f30d1c9d7e53f6c
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 17:41:01 -0700

    created function that adds 2 integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ *add- adds two integers
+ *@a: 1st number to add
+ *@b: 2nd number to add
+ *Return: result
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int add(int a, int b)
+{
+	result = a + b;
+	return (result);
+}
