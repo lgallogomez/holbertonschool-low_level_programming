9a18f0fcd788fe1b8613bf46fc0189a0b81350ec
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 18:36:30 -0700

    tring with if statements at < > cases

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	{
 		printf("%d\n", n);
 	}
-	while (n < 98)
+	while (n <= 98)
 	{
 		if (n == 98)
 		{
@@ -22,7 +22,7 @@ void print_to_98(int n)
 		printf("%d, ", n);
 		n++;
 	}
-	while (n > 98)
+	while (n >= 98)
 	{
 		if (n == 98)
 		{
