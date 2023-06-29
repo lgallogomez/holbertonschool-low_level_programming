b9155ab1a30cee4c5b7409e29b4acaba3cafbae5
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 18:44:22 -0700

    erased == 98 cases

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,9 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-	       	printf("%d, ", n);
+	       	printf("%d", n);
+		printf(" ");
 		n++;
-		if (n == 98)
-		{
-			printf("%d", n);
-		}
 	}
 	while (n >= 98)
 	{
