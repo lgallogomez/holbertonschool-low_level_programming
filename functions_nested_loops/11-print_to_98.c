805ed17ebc24dc2645b56777b592b9da349117e6
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 18:23:37 -0700

    trying with printf for all cases

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,16 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		printf("%d, ", n);
+		printf("%d\n", n);
 	}
 	while (n < 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+	        printf("%d, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 		n--;
 	}
 }
