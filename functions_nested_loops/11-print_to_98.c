0a21f5105a8137a19a08cd28824c04d5301895aa
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 17:53:37 -0700

    created function that prints argument until 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+/**
+ *print_to_98 - prints argument passed until 98
+ *@n: number to start printing until 98
+ *Return: 0
+ */
+
+#include <stdio.h>
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar('\n');
+		n++;
+	}
+	return (0);
+}
