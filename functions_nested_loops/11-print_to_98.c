c2de52ff4d5d95e0cb999746e374d84aec9e93ce
lgallogomez <l.gallogomez@gmail.com>
2023-06-28 17:56:14 -0700

    added "main.h> header file, return value updated to nothing and new line changed to space

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,19 @@
 /**
  *print_to_98 - prints argument passed until 98
  *@n: number to start printing until 98
- *Return: 0
+ *Return: nothing
  */
 
 #include <stdio.h>
+#include "main.h"
+
 void print_to_98(int n)
 {
 	while (n < 98)
 	{
 		_putchar(n + '0');
 		_putchar(',');
-		_putchar('\n');
+		_putchar(' ');
 		n++;
 	}
 	return (0);
