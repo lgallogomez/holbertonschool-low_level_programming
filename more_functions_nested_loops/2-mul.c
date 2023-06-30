b1d35319a7f6d3231e8dca017bdc242740ed842c
lgallogomez <l.gallogomez@gmail.com>
2023-06-30 08:51:53 -0700

    created the function that multiplies 2 integers and returns the result

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ *mul - function multiplies 2 integers
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the result
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
