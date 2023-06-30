2dbf953bb6309a1c5f360aa466090e661caaa396
lgallogomez <l.gallogomez@gmail.com>
2023-06-30 09:33:46 -0700

    created function that prints 0-9 but 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+/**
+ *print_numbers - prints numbers from 0 to 9 but 2 and 4
+ *Return: 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+	n++;
+}
