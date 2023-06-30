6c8853a860a35ca915bd898f55849d6e566433fb
lgallogomez <l.gallogomez@gmail.com>
2023-06-30 08:27:51 -0700

    created main.h header file with my own function prototypes

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+/**
+ *_isupper - checks for uppercase character
+ *@c: character to check is is uppercase
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+
+#include <stdio.h>
+#include "main.h"
+
