c0585362be72e1e5582345e110d0e55182d6631d
lgallogomez <l.gallogomez@gmail.com>
2023-06-30 12:57:52 -0700

    erased ; from else if

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+/**
+ *main - prints 1 - 100, if mult 3 Fizz, if mult 5 Buzz
+ *Return: 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			n++;
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+			n++;
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+			n++;
+		}
+		else
+		{
+			printf("%d ", n);
+			n++;
+		}
+	}
+}
