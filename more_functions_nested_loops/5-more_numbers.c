1dc8193d4e93e7af148b8bbdc4e0244906d6fba9
lgallogomez <l.gallogomez@gmail.com>
2023-06-30 14:58:19 -0700

    added if conditional for case if n > 9 to print decimals and then units of every number in just 2 steps

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+/**
+ *more_numbers - prints 10 times 0 to 14
+ *Return: 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void more_numbers(void)
+{
+	int counter = 0;
+	int n = 0;
+
+	while (counter <= 10)
+	{
+		while (n <= 14)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+			n++;
+		}
+		_putchar('\n');
+		counter++;
+	}
+	_putchar('\n');
+	return (0);
+}
