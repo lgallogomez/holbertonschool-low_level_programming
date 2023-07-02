aa44523a4ad07ea5d6a66410a600e7bf41084db2
lgallogomez <l.gallogomez@gmail.com>
2023-07-02 11:26:27 -0700

    included description to parameter

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,6 @@
 /**
  *reset_to_98 - takes a pointer and resets the value it points to to 98
+ *@*n: pointer that hold the address of the variable where the value to reset is
  *Return: doesnt return
  */
 
