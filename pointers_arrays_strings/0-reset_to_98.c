325f5f55ce2a22d436b7cf93dae5c01c3045afa3
lgallogomez <l.gallogomez@gmail.com>
2023-07-02 11:23:59 -0700

    erased main function and included function description

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
+/**
+ *reset_to_98 - takes a pointer and resets the value it points to to 98
+ *Return: doesnt return
+ */
 
+#include <stdio.h>
+#include "main.h"
 void reset_to_98(int *n)
 {
 	 *n = 98;
-}
-
-
-int main(void)
-{
-    int a;
-
-    a = 402;
-    printf("a=%d\n", a);
-    reset_to_98(&a);
-    printf("a=%d\n", a);
-    return (0);
 }
