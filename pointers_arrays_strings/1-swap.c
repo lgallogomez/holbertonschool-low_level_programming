0df8f9b97368840213d97b88702dc771f716ffa2
lgallogomez <l.gallogomez@gmail.com>
2023-07-02 20:21:09 -0700

    erased main function

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,16 +8,3 @@ void swap_int(int *a, int *b)
 	*b = *a;
 	*a = temp;
 	}
-
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
-}
