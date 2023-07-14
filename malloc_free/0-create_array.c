7b0080826e02b4119d4622f44efed1a093043a4c
lgallogomez <l.gallogomez@gmail.com>
2023-07-14 12:37:36 -0700

    created function and main.h header file

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+/**
+ *create_array - creates array of chars
+ *@size: size of memory block to allocate
+ *@c: char to initialize the memory block
+ *Return: pointer to array, NULL if size 0 or fails
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+	char *string;
+	int index = 0;
+
+	string = malloc(sizeof(c) * (size + 1);
+			if (size == 0)
+			{
+				return (NULL);
+			}
+			if (string == NULL);
+			{
+				return (NULL);
+			}
+			while (index < size)
+			{
+				string[index] = c;
+				index++;
+			}
+			string[index] = '\0';
+			return (string)
+			free (string);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+In this project i will be working on excercises related to the malloc function in the C programming language. 
