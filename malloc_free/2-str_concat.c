bd095cb8c354a099de2c0b57513efd0ac78b0b86
lgallogomez <l.gallogomez@gmail.com>
2023-07-19 13:53:03 -0700

    added or at if statements to manage null

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,7 +23,8 @@ if (s1 == NULL && s2 == NULL)
 }
 else if (s1 == NULL || s1[counter1] == '\0')
 {
-	for (counter2 = 0; s2[counter2] != '\0'; counter2++);
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
+		;
 	newspace = malloc(sizeof(char) * counter2 + 1);
 	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 	{
@@ -35,7 +36,8 @@ else if (s1 == NULL || s1[counter1] == '\0')
 }
 else if (s2 == NULL || s2[counter2] == '\0')
 {
-        for (counter1 = 0; s1[counter1] != '\0'; counter1++);
+        for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+		;
 	newspace = malloc(sizeof(char) * counter1 + 1);
 	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
 	{
@@ -46,41 +48,28 @@ else if (s2 == NULL || s2[counter2] == '\0')
 	return (newspace);
 }
 else
-	for (counter1 = 0; s1[counter1] != '\0'; counter1++);
-counter1--;
-for (counter2 = 0; s2[counter2] != '\0'; counter2++);
-space_for_concat = counter1 + 1 + counter2 + 1;
-newspace = malloc(sizeof(char) * (space_for_concat));
-if (newspace == NULL)
 {
-	return (NULL);
-}
-for (counter1 = 0; s1[counter1] != '\0'; counter1++)
-{
-	newspace[counter_newspace] = s1[counter1];
-	counter_newspace++;
-}
-for (counter2 = 0; s2[counter2] != '\0'; counter2++)
-{
-	newspace[counter_newspace] = s2[counter2];
-	counter_newspace++;
-}
-newspace[counter_newspace] = '\0';
-return (newspace);
-}
-
-
-int main(void)
-{
-	char *s;
-
-	s = str_concat("", "Hello");
-	if (s == NULL)
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+		;
+	counter1--;
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
+		;
+	space_for_concat = counter1 + 1 + counter2 + 1;
+	newspace = malloc(sizeof(char) * (space_for_concat));
+	if (newspace == NULL)
 	{
-		printf("failed\n");
-		return (1);
+		return (NULL);
 	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+	{
+		newspace[counter_newspace] = s1[counter1];
+		counter_newspace++;
+	}
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
+	{
+		newspace[counter_newspace] = s2[counter2];
+		counter_newspace++;
+	}
+	newspace[counter_newspace] = '\0';
+	return (newspace);
 }
