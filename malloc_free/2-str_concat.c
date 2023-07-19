84aa9aa19eea507f8d1cb7a60dbbab357476dc42
lgallogomez <l.gallogomez@gmail.com>
2023-07-19 11:24:58 -0700

    changed while loops into for loops to reduce # of lines

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,16 +5,12 @@
  * Return: concatenated string
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
+#include <stdlib.h>; #include <stdio.h>; #include "main.h";
+
 char *str_concat(char *s1, char *s2)
 {
-int counter1 = 0;
-int counter2 = 0;
+int counter1, counter2, space_for_concat, counter_newspace;
 char *newspace = NULL;
-int space_for_concat = 0;
-int counter_newspace = 0;
 
 if (s1 == NULL && s2 == NULL)
 {
@@ -22,77 +18,49 @@ if (s1 == NULL && s2 == NULL)
 	newspace[counter_newspace] = '\0';
 	return (newspace);
 }
-
-
 else if (s1 == NULL)
 {
-	while (s2[counter2] != '\0')
-	{
-		counter2++;
-	}
+	for (counter2 = 0; s2[counter2] != '\0'; counter++);
 	newspace = malloc(sizeof(char) * counter2 + 1);
-	counter2 = 0;
-	while (s2[counter2] != '\0')
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 	{
 		newspace[counter_newspace] = s2[counter2];
 		counter_newspace++;
-		counter2++;
 	}
 	newspace[counter_newspace] = '\0';
 	return (newspace);
 }
-
-
 else if (s2 == NULL)
 {
-	while (s1[counter1] != '\0')
-	{
-		counter1++;
-	}
+        for (counter1 = 0; s1[counter1] != '\0'; counter1++);
 	newspace = malloc(sizeof(char) * counter1 + 1);
-	counter1 = 0;
-	while (s1[counter1] != '\0')
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++;)
 	{
 		newspace[counter_newspace] = s1[counter1];
 		counter_newspace++;
-		counter1++;
 	}
 	newspace[counter_newspace] = '\0';
 	return (newspace);
 }
-
-
 else
-	while (s1[counter1] != '\0')
-	{
-		counter1++;
-	}
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++);
 counter1--;
-
-while (s2[counter2] != '\0')
-{
-	counter2++;
-}
+for (counter2 = 0; s2[counter2] != '\0'; counter2++);
 space_for_concat = counter1 + 1 + counter2 + 1;
 newspace = malloc(sizeof(char) * (space_for_concat));
 if (newspace == NULL)
 {
 	return (NULL);
 }
-
-counter1 = 0;
-counter2 = 0;
-while (s1[counter1] != '\0')
+for (counter1 = 0; s1[counter1] != '\0'; counter1++)
 {
 	newspace[counter_newspace] = s1[counter1];
 	counter_newspace++;
-	counter1++;
 }
-while (s2[counter2] != '\0')
+for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 {
 	newspace[counter_newspace] = s2[counter2];
 	counter_newspace++;
-	counter2++;
 }
 newspace[counter_newspace] = '\0';
 return (newspace);
