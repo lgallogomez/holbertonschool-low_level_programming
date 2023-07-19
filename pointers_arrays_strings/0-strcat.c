77c1d181f4ffd5395bc44342e5adc844e9e67b87
lgallogomez <l.gallogomez@gmail.com>
2023-07-19 16:11:57 -0700

    created and teste function

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,3 +8,16 @@
 #include <stdio.h>
 #include "main.h"
 
+char *_strcat(char *dest, char *src)
+{
+	int src_counter, dest_counter;
+
+	for (dest_counter = 0; dest[dest_counter] != '\0'; dest_counter++)
+		;
+	for (src_counter = 0; src[src_counter] != '\0'; src_counter++)
+	{
+		dest[dest_counter] = src[src_counter];
+		dest_counter++;
+	}
+	return (dest);
+}
