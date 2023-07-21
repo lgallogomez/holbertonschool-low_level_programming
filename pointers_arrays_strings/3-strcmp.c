961e018194e3e876a701388b3040a94b94f1f57e
lgallogomez <l.gallogomez@gmail.com>
2023-07-20 19:09:21 -0700

    if equal statement moved to the beginning of function

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,20 @@ int _strcmp(char *s1, char *s2)
 int cs1 = 0;
 int cs2 = 0;
 
-for (cs1 = 0, cs2 = 0; s1[cs1] != '\0' && s2[cs2] != '\0'; cs1++, cs2++)
-{
-	if (s1[cs1] == s2[cs2])
+if (s1 == s2)
+	return (0);
+else
+	for (cs1 = 0, cs2 = 0; s1[cs1] != '\0' && s2[cs2] != '\0'; cs1++, cs2++)
 	{
-		;
+		if (s1[cs1] == s2[cs2])
+		{
+			;
+		}
+		else if (s1[cs1] < s2[cs2])
+			return (-15);
+		else if (s1[cs1] > s2[cs2])
+		{
+			return (15);
+		}
 	}
-	else if (s1[cs1] < s2[cs2])
-		return (-15);
-	else if (s1[cs1] > s2[cs2])
-	{
-		return (15);
-	}
-}
-return (0);
 }
