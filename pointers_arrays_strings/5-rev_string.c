3760ce7d53b14dbc1253d2870a0fb4d39419e3f7
lgallogomez <l.gallogomez@gmail.com>
2023-07-25 15:31:09 -0700

    Rev string function created

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *rev_string - reverse string
+ *@s: string to reverse
+ *Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int it1 = 0;
+	int it2 = 0;
+	char temp[1000];
+
+	for (; s[it1] != '\0'; it1++)
+	{
+		temp[it1] = s[it1];
+	}
+	it1--;
+	for (; s[it2] != '\0'; it2++)
+	{
+		s[it2] = temp[it1];
+		it1--;
+	}
+	return (0);
+}
