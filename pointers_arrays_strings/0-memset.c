d9a8e0c49239331a9e81474599f446d13b9267e4
lgallogomez <l.gallogomez@gmail.com>
2023-07-29 16:20:51 -0700

    created memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memset - fills memory with a constant byte
+ *@s: pointer to memory to fill
+ *@b: byte to fill s
+ *@n: number of bytes
+ *Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
