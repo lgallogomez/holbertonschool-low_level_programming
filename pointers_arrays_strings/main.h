7ee47b949180569f640453168b8375a25c152390
lgallogomez <l.gallogomez@gmail.com>
2023-07-29 16:22:33 -0700

    added memset function prototype to main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
