02b1ca592844ca653e19d933555159471cf4a5d8
lgallogomez <l.gallogomez@gmail.com>
2023-07-31 09:00:22 -0700

    updated header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,19 @@
-#ifndef struct
-#define struct
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - strcutures with dogs identity
+ *@name: dogs name
+ *@age: dogs age
+ *@owned: dogs owner
+ */
+
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
