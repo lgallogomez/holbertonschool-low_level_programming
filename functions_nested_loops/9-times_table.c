3bb04a16ba08703c6573531011c34ccb9abfcb0b
lgallogomez <l.gallogomez@gmail.com>
2023-08-01 15:59:27 -0700

    changed logic of 9-times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
         int j = 0;
         int mult;
 
-	for (; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(; j < 10; j++)
+		for(j = 0; j < 10; j++)
 		{
 			mult = i * j;
 			if (mult > 9)
