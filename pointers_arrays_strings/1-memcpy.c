7f35bb16e8236014abe833a0ca94cbc6c14af896
lgallogomez <l.gallogomez@gmail.com>
2023-08-04 14:16:31 -0700

    description added to return value and spaces for parenthesis added

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,13 +3,14 @@
  *@dest: memory that will hold a copy of memory
  *@src: memory to copy mem from
  *@n: number of bytes to copy
- */
+ *Return: pointer to memory space copied
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for(; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = src[i];
-	return(dest);
+	return (dest);
 }
