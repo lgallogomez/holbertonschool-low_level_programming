239f7858f6e2f20df77655dde42a795ca74543d4
lgallogomez <l.gallogomez@gmail.com>
2023-08-05 10:17:06 -0700

    created function 8 of print square, main function and executable file to test

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+/**
+ *print_square - prints a square
+ *@size: size of square
+ *Return: nothing
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+	
+	if (size <= 0)
+		_putchar('\n');
+	for (; i < size; i++)
+	{
+		j = 0;
+		for(; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
