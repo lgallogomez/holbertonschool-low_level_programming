efeecfe663912e8ba46ecd9b0e0fdc2e05c8a321
lgallogomez <l.gallogomez@gmail.com>
2023-08-09 14:11:13 -0700

    sending it to test 1-print n of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+/**
+ *main- prints number of argumnets passed to it
+ *@argc: counts number of arguments
+ *@argv: array with arguments value
+ *Return: 0 if success, other if error
+ */
+
+#include <stdio.h>
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
