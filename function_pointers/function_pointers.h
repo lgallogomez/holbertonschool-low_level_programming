f118a6020ad3e1d6af3ae12f127e72341769b73c
Luis Gallo <l.gallogomez@gmail.com>
2023-08-11 12:39:16 -0500

    added ifndef, define and endif in header file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
+#endif
