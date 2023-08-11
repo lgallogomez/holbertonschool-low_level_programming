10f2b73ecba722caecaf34b6f17046f0a31fdf7e
Luis Gallo <l.gallogomez@gmail.com>
2023-08-11 12:25:53 -0500

    created and tested 1st excercise of function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+*print_name- prints a name
+*@name: pointer to char holds a name
+*@f: point er to function that takes a pointer to char
+*Return: nothing
+*/
+
+#include <stdio.h>
+
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+int _putchar(char c);
