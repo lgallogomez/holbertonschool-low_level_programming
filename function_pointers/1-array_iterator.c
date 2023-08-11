c8168df228aab8321d9e96a2d87a8038d5098c91
Luis Gallo <l.gallogomez@gmail.com>
2023-08-11 14:22:10 -0500

    added case when function action is NULL

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,23 +1,23 @@
 /**
-*array_iterator - executes a function given as a parameter on each element
-*@array: array to iterate
-*@size: size of array
-*@action: pointer to function to use
-*Return: nothing
-*/
+ *array_iterator - executes a function given as a parameter on each element
+ *@array: array to iterate
+ *@size: size of array
+ *@action: pointer to function to use
+ *Return: nothing
+ */
 
 #include <stdio.h>
 #include "function_pointers.h"
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    long unsigned int i = 0;
+	long unsigned int i = 0;
 
-    if (size == 0 || array == NULL)
-        return;
+	if (size == 0 || array == NULL || action == NULL)
+		return;
 
-    for(; i < size; i++)
-    {
-        action(array[i]);
-    }
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
