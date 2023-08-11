78a04e517f3a42a96370081e7c12bf171e7ec23c
Luis Gallo <l.gallogomez@gmail.com>
2023-08-11 15:43:13 -0500

    created, tested int_index and added description of function

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,13 +1,21 @@
+/**
+ * int_index - function that searches for an int
+ * @array: array of integers where search is done
+ * @size: size of array
+ * @cmp: pointer to function that compares
+ * Return: index where function found especific integer
+*/
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-		return (-1);        
-	for (; i < size;i++)
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-	return (-1);    
+	return (-1);
 }
