405421ed1330a630930686ec7aec1c1558a37a34
Luis Gallo <l.gallogomez@gmail.com>
2023-08-13 23:13:46 -0500

    i++ outside if in while loop

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,22 +11,22 @@ int main(int argc, char *argv[])
 {
 int second_num, first_num, result;
 
-if (argc != 4)
-{
-	printf("Error\n");
-	exit(98);
-}
-if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
-	&& *argv[2] != '/' && *argv[2] != '%')
-{
-	printf("Error\n");
-	exit(99);
-}
-if ((*argv[2] == '%' || *argv[2] == '/') && *argv[3] == '0')
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(98);
 	}
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+		&& *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '%' || *argv[2] == '/') && *argv[3] == '0')
+		{
+			printf("Error\n");
+			exit(100);
+		}
 first_num = atoi(argv[1]);
 second_num = atoi(argv[3]);
 result = get_op_func(argv[2])(first_num, second_num);
