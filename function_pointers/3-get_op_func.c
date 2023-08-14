7838ff8c6327ad8ebb962d48204c3e9bd0d61cda
Luis Gallo <l.gallogomez@gmail.com>
2023-08-13 22:13:48 -0500

    created and tested function, * doesnt work but i want to try in the intranet checkers

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+#include "3-calc.h"
+int (*get_op_func(char *s))(int, int)
+{
+	 int i;
+	 
+	 op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	for (i = 0; i < 5; i++)
+	{
+	if (ops[i].op[0] == s[0])
+		return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,34 @@
+/**
+ * main- performs simple aritmetic operations
+ * @argc: counts number of arguments passed
+ * @argv: array of pointers with values passed
+ * Return: nothing 
+*/
+
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+int second_num, first_num, result;
+
+if (argc != 4)
+{
+	printf("Error\n");
+	exit(98);
+}
+if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+{
+	printf("Error\n");
+	exit(99);
+}
+if ((*argv[2] == '%'|| *argv[2] == '/') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+first_num = atoi(argv[1]);
+second_num = atoi(argv[3]);
+result = get_op_func(argv[2])(first_num, second_num);
+printf("%d\n", result);
+return (0);
+}
