471bf3e6f2863c591ee798c8f205614b3efadd5b
Luis Gallo <l.gallogomez@gmail.com>
2023-08-13 23:04:47 -0500

    i++ outside if in while loop

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,12 +16,13 @@ if (argc != 4)
 	printf("Error\n");
 	exit(98);
 }
-if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/'
+/ && *argv[2] != '%')
 {
 	printf("Error\n");
 	exit(99);
 }
-if ((*argv[2] == '%'|| *argv[2] == '/') && *argv[3] == '0')
+if ((*argv[2] == '%' || *argv[2] == '/') && *argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
